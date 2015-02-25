906adbb5039d9a854c0f9524f2ba55d236bbabaf
goodjob1114 <goodjob1114@gmail.com>
2015-02-25 23:44:06 +0800

    intall ruby env on ubuntu1404 by rbenv

---
+++ ubuntu-14.04/ruby-install-by-rbenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+
+curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+
+bashrc="~/.bashrc"
+
+cat >> $bashrc << EOF
+export RBENV_ROOT="${HOME}/.rbenv"
+if [ -d "${RBENV_ROOT}" ]; then
+  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  eval "$(rbenv init -)"
+fi
+EOF
+
+source $bashrc
+
+rbenv install 2.1.5
+rbenv global 2.1.5
+ruby -v
